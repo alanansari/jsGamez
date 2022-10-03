3a4242e512b775e8cb1f53c28d82a72583bb8ceb
Alan <ansarialan31@gmail.com>
2022-10-04 02:59:11 +0530

    Added Tail Generator Method

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let posx = 0, posy =  0;
+let prevx=0,prevy=0;
+let currx=0,curry=0;
 let up=0,left=0,right=1,down=0;
 let pause = 0, score=0;
 const pausebtn = document.getElementById("pausebtn");
@@ -12,10 +14,40 @@ let myInterval;
 startgame();
 
 function startgame(){
-    myInterval = setInterval(nextframe,100);
+    myInterval = setInterval(nextframe,120);
+}
+
+function incLength(length){
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id","tail-"+(length-1));
+    newDiv.setAttribute("class","tail");
+    const addNode = document.getElementById("console");
+    addNode.appendChild(newDiv);
+}
+
+function updateTailPos(length){
+    for(let i=0;i<length;i++){
+        if(i==0){
+            const x = document.getElementById("tail-"+0);
+            x.style.top = posy + "px";
+            x.style.left = posx + "px";
+            prevx=posx;
+            prevy=posy;
+        }else{
+            const curr = document.getElementById("tail-"+i);
+            currx=parseInt(curr.style.left.slice(0,-2),10);
+            curry=parseInt(curr.style.top.slice(0,-2),10);
+            curr.style.left = prevx + "px";
+            curr.style.top = prevy + "px";
+            prevx=currx;
+            prevy=curry;
+        }
+    }
 }
 
 function nextframe(){
+
+    updateTailPos(score);
 
     //              continuous movement
     if(up===1)
@@ -46,10 +78,13 @@ function nextframe(){
         food.style.left = foodx + "px";
 
         score++;
-        let outscore = document.getElementById("score");
+        incLength(score);
+
+        const outscore = document.getElementById("score");
         outscore.innerHTML = "Score: "+score;
     }
 
+    // Position the head
     const head = document.getElementById("head");
     head.style.top = posy + "px";
     head.style.left = posx + "px";
