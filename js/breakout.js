ec1983e2f39ea9f9f992dfc992a2bcf01eb44e6c
Alan <ansarialan31@gmail.com>
2022-10-05 06:05:57 +0530

    Added moving ball and border collision detection

--- js/breakout.js
+++ js/breakout.js
@@ -1,28 +1,45 @@
 const console = document.querySelector('.console');
 const heading = document.getElementById('heading');
 const pad = document.getElementById('pad');
-let posX=0,posY=0;
+const ball = document.getElementById('ball');
+let posX=0,posY=0,ballX=375,ballY=375;
+let xDir = 1,yDir = -1;
 
 let myInterval = setInterval(game,5);
 
-for(let i=0;i<=5;i++){
-    for(let j=0;j<16;j++){
+for(let i=0;i<=3;i++){
+    for(let j=0;j<8;j++){
         const newBlock = document.createElement("div");
         newBlock.setAttribute("class","block");
         newBlock.setAttribute("id","block-"+i+'-'+j);
         const addBlock = document.querySelector('.console');
-        newBlock.style.left = j*50 + 'px';
-        newBlock.style.top = i*20 + 'px';
+        newBlock.style.left = j*100 + 'px';
+        newBlock.style.top = i*40 + 'px';
         addBlock.appendChild(newBlock);
     }
 }
 
 function game(){
-    blockGen();
+    ballGen();
 }
 
-function blockGen(){
-    
+function ballGen(){
+    ballX+=xDir;
+    ballY+=yDir;
+    checkCollision();
+    ball.style.top = ballY + 'px';
+    ball.style.left = ballX + 'px';
+}
+
+function checkCollision(){
+    if(ballX<=0)
+        xDir=1;
+    if(ballX>=775)
+        xDir=-1;
+    if(ballY<=0)
+        yDir=1;
+    if(ballY>=425)
+        yDir=-1;
 }
 
 function getCursorPos(event){
